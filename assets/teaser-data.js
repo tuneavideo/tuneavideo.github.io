68b96810bf2c73e6c2d9fc8fedf4cb9d2ba5872f
zhangjiewu <zhangjiewu.cn@gmail.com>
2023-03-18 01:32:40 +0800

    update results

--- assets/teaser-data.js
+++ assets/teaser-data.js
@@ -8,14 +8,14 @@ file_paths = {
     "car-moving-porsche": "./assets/results/tuneavideo/car-turn/porsche-beach.gif",
 
     "basketball-trump": "./assets/results/tuneavideo/man-basketball/trump.gif",
-    "basketball-ironman": "./assets/results/tuneavideo/man-basketball/ironman.gif",
-    "basketball-monkey": "./assets/results/tuneavideo/man-basketball/monkey.gif",
+    "basketball-lego": "./assets/results/tuneavideo/man-basketball/lego.gif",
+    "basketball-astronaut": "./assets/results/tuneavideo/man-basketball/astronaut.gif",
 
     "lion-vangogh": "./assets/results/tuneavideo/lion-roaring/lion-vangogh.gif",
     "lion-tiger": "./assets/results/tuneavideo/lion-roaring/tiger-roar.gif",
     "lion-wolf": "./assets/results/tuneavideo/lion-roaring/wolf-nyc.gif",
 
-    "rabbit-pizza": "./assets/results/tuneavideo/rabbit-watermelon/rabbit-pizza.gif",
-    "rabbit-puppy": "./assets/results/tuneavideo/rabbit-watermelon/puppy-orange.gif",
-    "rabbit-tiger": "./assets/results/tuneavideo/rabbit-watermelon/tiger-watermelon.gif",
+    "rabbit-alone": "./assets/results/tuneavideo/rabbit-watermelon/rabbit.gif",
+    "rabbit-puppy": "./assets/results/tuneavideo/rabbit-watermelon/puppy.gif",
+    "rabbit-cat": "./assets/results/tuneavideo/rabbit-watermelon/cat.gif",
 }
